f8a725fa26e29ab988b63395acfa774dfb593e6e
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-03-19 12:30:45 -0700

    Lunch commit!

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,38 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  Promise.resolve(req.cookies['shortlyid'])
+    .then((result) => {
+      console.log('I am here!');
+    })
+    .catch(() => {
+      return models.Sessions.create()
+        .then((session) => {
+          console.log('I am over here!');
+          let id = session.userId;
+          return models.Session.get({ id: id });
+        })
+        .then((result) => {
+          req.session = result;
+          next();
+        });
+    });
+  // .then((data) => {
+  //   console.log(data);
+  // })
+  // .then((session) => {
+  //   req.session = session;
+  //   next();
+  // });
+  // Promise.resolve(req.cookies)
+  //   .then(result => {
+
+  //   });
+  // console.log(req.cookies);
+  // if (!req.cookies) {
+  // models.Sessions.create();
+  // }
+  // next();
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  if (req.headers.cookie) {
+    let cookieArr = req.headers.cookie.split('; ');
+    cookieArr.forEach(cookie => {
+      var kvCookie = cookie.split('=');
+      req.cookies[kvCookie[0]] = kvCookie[1];
+    });
+  } else {
+    req.cookies = {};
+  }
+  next();
 };
 
 module.exports = parseCookies;
